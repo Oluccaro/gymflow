131c7d4b903aea90545e5946a8b3231c985bc1ad
Caroline Prockmann <carolprock@gmail.com>
2024-06-18 14:00:39 -0300

    att

--- app/(tabs)/finances.tsx
+++ app/(tabs)/finances.tsx
@@ -2,6 +2,8 @@ import ListFinance from '../screens/finance/list-finance'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 
+const Stack = createNativeStackNavigator();
+
 const Finances = () => {
   return (
    <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
